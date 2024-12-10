8c9929c82c8b868009d78d5064a2cb32ed07c916
최현수 <assam258@gmail.com>
2024-12-10 15:34:29 +0900

    데이터 생성 스크립트 수정

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@
 
 def main():
     print("FT.CREATE publicT1Idx ON hash PREFIX 1 public.t1: SCHEMA i1 NUMERIC i2 NUMERIC SORTABLE i3 TEXT")
-    for i in range(0, 1000000):
+    for i in range(0, 10000000):
         print(f"HSET public.t1:{i} i1 {i} i2 {i//10000} i3 TEXT_{i}")
     print("SAVE")
 
